27f88ba464400622853626fd042b2280317f68c8
Paul <68791915+paulbqnt@users.noreply.github.com>
2024-07-29 18:55:36 +0200

    Add files via upload
    update init.el

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("bbb13492a15c3258f29c21d251da1e62f1abb8bbd492386a673dcfab474186af" "d7bf35cbf07fe90b420ca85625d4e1baff08fd64282562dde9dc788ed89c8242" default))
- '(package-selected-packages '(spacemacs-theme evil)))
+ '(package-selected-packages '(org-journal spacemacs-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -53,3 +53,53 @@
 (global-set-key (kbd "C-c c") 'org-capture)
 
 (setq org-agenda-files '("~/org/todo.org" "~/org/projects.org"))
+
+;; Install org-journal
+(unless (package-installed-p 'org-journal)
+  (package-install 'org-journal))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'org-journal)
+
+;; General org-journal settings
+(setq org-journal-date-format "%A, %Y-%m-%d")
+
+;; Function to switch to work journal
+(defun my-org-journal-work ()
+  (interactive)
+  (setq org-journal-dir "~/org/journal/work/")
+  (setq org-journal-file-format "%Y-%m-%d.org")
+  (message "Switched to Work Journal"))
+
+;; Function to switch to personal journal
+(defun my-org-journal-personal ()
+  (interactive)
+  (setq org-journal-dir "~/org/journal/personal/")
+  (setq org-journal-file-format "%Y-%m-%d.org")
+  (message "Switched to Personal Journal"))
+
+;; Keybindings to switch between journals
+(global-set-key (kbd "C-c j w") 'my-org-journal-work)
+(global-set-key (kbd "C-c j p") 'my-org-journal-personal)
+
+;; Keybinding to create a new journal entry (will use the current journal context)
+
+(global-set-key (kbd "C-c j j") 'org-journal-new-entry)
+(defun my-org-journal-work ()
+  (interactive)
+  (setq org-journal-dir "~/org/journal/work/")
+  (setq org-journal-file-format "%Y-%m-%d.org")
+  (setq org-journal-date-format "%A, %d %B %Y")
+  (setq org-journal-file-header "#+TITLE: Work Journal Entry\n#+STARTUP: showeverything\n\n")
+  (message "Switched to Work Journal"))
+
+(defun my-org-journal-personal ()
+  (interactive)
+  (setq org-journal-dir "~/org/journal/personal/")
+  (setq org-journal-file-format "%Y-%m-%d.org")
+  (setq org-journal-date-format "%A, %Y-%m-%d")
+  (setq org-journal-file-header "#+TITLE: Personal Journal Entry\n#+STARTUP: showeverything\n\n")
+  (message "Switched to Personal Journal"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
