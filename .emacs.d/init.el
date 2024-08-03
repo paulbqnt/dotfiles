97594a367852beb73b322a6ef60840b4b0311146
Paul <68791915+paulbqnt@users.noreply.github.com>
2024-08-03 23:27:25 +0200

    Add files via upload

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,67 +1,101 @@
-;;; Startup
-;;; PACKAGE LIST
-(setq package-archives 
-      '(("melpa" . "https://melpa.org/packages/")
-        ("elpa" . "https://elpa.gnu.org/packages/")))
+;; 8:16 https://www.youtube.com/watch?v=IspAZtNTslY 
+(setq inhibit-startup-message t)
 
-;;; BOOTSTRAP USE-PACKAGE
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+
+(menu-bar-mode -1)
+
+(setq visible-bell t)
+
+(set-face-attribute 'default nil :font "Fira Code" :height 20)
+
+(load-theme 'tango-dark)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+
+
+;; Initialize package sources
+(require 'package)
+
+
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
 (package-initialize)
-(setq use-package-always-ensure t)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
   (package-install 'use-package))
-(eval-when-compile (require 'use-package))
 
-;;; UNDO
-;; Vim style undo not needed for emacs 28
-(use-package undo-fu)
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-;;; Vim Bindings
-(use-package evil
-  :demand t
-  :bind (("<escape>" . keyboard-escape-quit))
-  :init
-  ;; allows for using cgn
-  ;; (setq evil-search-module 'evil-search)
-  (setq evil-want-keybinding nil)
-  ;; no vim insert bindings
-  (setq evil-undo-system 'undo-fu)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+
+
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
   :config
-  (evil-mode 1))
+  (ivy-mode 1))
 
-;;; Vim Bindings Everywhere else
-(use-package evil-collection
-  :after evil
-  :config
-  (setq evil-want-integration t)
-  (evil-collection-init))
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+
+
+
+
+
+
+
+
+
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" default))
  '(package-selected-packages
-   '(vertico evil-collection undo-fu use-package org-journal gruvbox-theme evil)))
+   '(doom-modeline vertico use-package undo-fu ivy gruvbox-theme evil-collection)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(use-package gruvbox-theme
-  :config
-  (load-theme 'gruvbox))
-
-(use-package vertico
-  :config
-  (vertico-mode))
-
-(require 'org-bullets)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-
-(setq org-journal-dir "~/org/journal/")
-(setq org-journal-date-format "%A, %d %B %Y")
-(require 'org-journal)
