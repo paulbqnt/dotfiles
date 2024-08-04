fd2b84a56b3399e7e2e650ec942e6521a10e7504
Paul <68791915+paulbqnt@users.noreply.github.com>
2024-08-04 19:44:36 +0200

    Add files via upload

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,9 +10,9 @@
 
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "Fira Code" :height 20)
+;;(set-face-attribute 'default nil :font "Fira Code" :height 20)
 
-(load-theme 'tango-dark)
+;;(load-theme 'tango-dark)
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -22,12 +22,13 @@
 ;; Initialize package sources
 (require 'package)
 
-
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
+
+
+ 
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -46,16 +47,14 @@
 (dolist (mode '(org-mode-hook
 		term-mode-hook
 		eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-
+  (add-hook mode (lambda () (display-line-numbers-mode 0)))) 
 
 
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
          :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
+         ("TAB" . ivy-alt-done)	
          ("C-l" . ivy-alt-done)
          ("C-j" . ivy-next-line)
          ("C-k" . ivy-previous-line)
@@ -69,30 +68,99 @@
   :config
   (ivy-mode 1))
 
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
+(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
+
+;;(define-key emacs-lisp-mode (kbd "C-x M-t") 'counsel-load-theme)
 
 
+(use-package all-the-icons)
 
 
+  (use-package doom-themes
+    :init (load-theme  'doom-palenight t))
+			     
+ (use-package doom-modeline
+   :init (doom-modeline-mode 1)
+   :custom ((doom-modeline-height 15)))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
 
 
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
+  
+
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap descriBe-key] . helpful-key))
+
+(use-package general
+:config
+(general-create-definer rune/leader-keys
+ "C-M-j" 'counsel-switch-
+  :keymaps '(normal insert visual emacs)
+  :prefix "SPC"
+  :global-prefix "C-SPC")
+
+(rune/leader-keys
+ "t" '(:ignore t :which-key "toggles")
+ ))
 
 
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
 
-
+(use-package evil-collection
+:after evil
+:config
+(evil-collection-init))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8a379e7ac3a57e64de672dd744d4730b3bdb88ae328e8106f95cd81cbd44e0b6" "dcb1cc804b9adca583e4e65755895ba0a66ef82d29464cf89a78b88ddac6ca53" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "7c28419e963b04bf7ad14f3d8f6655c078de75e4944843ef9522dbecfcd8717d" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" default))
  '(package-selected-packages
-   '(doom-modeline vertico use-package undo-fu ivy gruvbox-theme evil-collection)))
+   '(doom-modeline-now-playing ewal-doom-themes which-key vertico use-package undo-fu spacemacs-theme rainbow-delimiters org-journal org-bullets monokai-pro-theme ivy-rich helpful gruvbox-theme general evil-collection dracula-theme doom-themes doom-modeline counsel all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
